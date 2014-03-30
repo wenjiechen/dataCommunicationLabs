eee6ee4e1d755a54e1ab13a4c414aff7f4a147bb
Wenjie Chen <wenjie.chen@nyu.edu>
2014-03-30 11:23:16 -0400

    refactor server

--- src/webproxy/WorkerRunnable.java
+++ src/webproxy/WorkerRunnable.java
@@ -8,19 +8,22 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.Socket;
 import java.net.URL;
 
 public class WorkerRunnable implements Runnable {
 
-  protected Socket clientSocket = null;
-  protected String serverText = null;
-  protected Proxy proxy = null;
+  private Socket clientSocket = null;
+  private Proxy proxy = null;
+  private String proxyIp;
+  private int proxyPort;
 
-  public WorkerRunnable(Socket clientSocket, String serverText) {
+  public WorkerRunnable(Socket clientSocket, String proxyIp, int proxyPort) {
     this.clientSocket = clientSocket;
-    this.serverText = serverText;
+    this.proxyIp = proxyIp;
+    this.proxyPort = proxyPort;
   }
 
   public void run() {
@@ -35,32 +38,48 @@ public class WorkerRunnable implements Runnable {
 
   private void proxyHandler(InputStream inputStream, OutputStream outputStream)
       throws IOException {
+    URL url = getURL(inputStream);
+    if (url == null)
+      return;
+    System.out.println("url:\n" + url);
+    // instance remote proxy
+    proxy = new Proxy(Proxy.Type.HTTP,
+        new InetSocketAddress(proxyIp, proxyPort));
+    // use proxy open a web page
+    HttpURLConnection action = (HttpURLConnection) url.openConnection(proxy);
+    InputStream in = action.getInputStream();
+    BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+    StringBuilder sb = new StringBuilder();
+    String lin = System.getProperty("line.separator");
+    for (String temp = br.readLine(); temp != null; temp = br.readLine()) {
+      sb.append(temp + lin);
+    }
+    System.out.println("====get page===");
+    httpResponseConstructer(sb.toString(), outputStream);
+    br.close();
+    outputStream.close();
+    inputStream.close();
+  }
+
+  private URL getURL(InputStream inputStream) throws IOException {
     int count = 0;
     while (count == 0)
       count = inputStream.available();
     byte[] bytes = new byte[count];
     inputStream.read(bytes);
     String str = new String(bytes);
+    if (str.startsWith("GET") == false)
+      return null;
     String[] strs = str.split("\\n");
-    System.out.println("head:\n"+strs[0]);
+    System.out.println("head:\n" + strs[0]);
     String[] head = strs[0].split(" ");
-    proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("91.210.44.196",
-        3129)); // 实例化本地代理对象，端口为8888
-    URL url = new URL(head[1]);
-    System.out.println("url:\n"+url);
-    HttpURLConnection action = (HttpURLConnection) url.openConnection(proxy); // 使用代理打开网页
-    InputStream in = action.getInputStream();
-    BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-    StringBuilder sb = new StringBuilder();
-    String lin = System.getProperty("line.separator");
-    for (String temp = br.readLine(); temp != null; temp = br.readLine()) {
-      sb.append(temp + lin);
+    URL url = null;
+    try {
+      url = new URL(head[1]);
+    } catch (MalformedURLException e) {
+      throw new MalformedURLException(" ");
     }
-    System.out.println(sb.toString());
-    httpResponseConstructer(sb.toString(), outputStream);
-    br.close();
-    outputStream.close();
-    inputStream.close();
+    return url;
   }
 
   private void httpResponseConstructer(String response, OutputStream output)
